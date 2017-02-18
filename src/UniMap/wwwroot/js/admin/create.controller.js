0f2a4451b323570e9c0d4bbc157a8010458f0609
Patrick Tone <tone@thepat.xyz>
2017-02-18 13:53:31 -0600

    TODO: Fix admin pages

--- src/UniMap/wwwroot/js/admin/create.controller.js
+++ src/UniMap/wwwroot/js/admin/create.controller.js
@@ -1,8 +1,8 @@
 ﻿/// <reference path="../lib/angular/angular.js" />
 
 angular.module('appModule')
-    .controller('createCtrl', ['$scope', 'eventService',
-        function ($scope, eventService) {
+    .controller('createCtrl', ['$scope', '$mdDialog', 'eventService', 'mapService',
+        function ($scope, $mdDialog, eventService, mapService) {
             $scope.newEvent = {};
 
             $scope.createEvent = () => {
@@ -12,5 +12,17 @@ angular.module('appModule')
                         console.log($scope.newEvent);
                     });
             }
+
+            $scope.initMap = () => {
+                $scope.$apply(() => {
+                    var myLocation = new google.maps.LatLng(33.607601, -101.936684);
+                    $scope.map = new google.maps.Map(document.getElementById('small_map'), {
+                        center: myLocation,
+                        zoom: 17
+                    });
+                });
+            }
+
+            mapService.init();
         }
     ]);
