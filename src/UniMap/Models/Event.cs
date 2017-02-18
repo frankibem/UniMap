86016fd6b42a9e64fea424549fa8c571bab1b161
Patrick Tone <tone@thepat.xyz>
2017-02-17 22:30:21 -0600

    Added the base models.

---
+++ src/UniMap/Models/Event.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniMap.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public DateTime StartOn { get; set; }
+
+        public DateTime EndOn { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
