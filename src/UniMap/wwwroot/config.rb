bad8f4ae95f6f5831673c072e0350fc0563066fc
mariot <mario@teamhone.io>
2017-02-18 04:05:15 -0600

    Added menu to admin, and updated styles

--- src/UniMap/wwwroot/config.rb
+++ src/UniMap/wwwroot/config.rb
@@ -1,4 +1,5 @@
 require 'compass/import-once/activate'
+require 'autoprefixer-rails'
 
 # Require any additional compass plugins here.
 
@@ -24,3 +25,19 @@ output_style = :compressed
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  map = file + '.map'
+
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+      from: file,
+      to:   file,
+      map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
+  end
+end
